2db32e6a60eb7d20d0dc4f429bf2d56070e20c8f
jloope <james.loope@nike.com>
2014-07-22 15:52:43 -0700

    figure out which in-use volumes have snapshots

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"][amazonica "0.2.21"]]
+  :dependencies [[org.clojure/clojure "1.6.0"][amazonica "0.2.21"][clj-time "0.7.0"]]
   :main ^:skip-aot aws-snapshot-monkey.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/aws_snapshot_monkey/core.clj
+++ src/aws_snapshot_monkey/core.clj
@@ -1,22 +1,36 @@
 (ns aws-snapshot-monkey.core
   (:gen-class)
+  (:require [clj-time.core :as tm]
+            [clj-time.local :as loc]
+            [clj-time.format :as fmt])
+  (:use clj-time.coerce)
   (:use [amazonica.aws.ec2])
-  (:use [clojure.pprint]))
+  (:use [clojure.pprint])
+  (:use [clojure.set]))
 
 (defn get_volumes_in_use [] 
   "grab the aws volume-id's for in-use volumes"
-        (map 
-          (fn [m] (get m :volume-id))
-          (get (describe-volumes :state "in-use") :volumes)
-          ))
+  (map 
+    (fn [m] (get m :volume-id))
+    (get (describe-volumes :state "in-use") :volumes)
+    ))
 
 (defn get_snapshots_for_volumes [volumes]
-  "fetch the snapshot-id's for a given volumes"
-  (describe-snapshots :filters [{:name "volume-id" :values volumes}])
-)
+  "fetch the snapshot-id's and volume-id's for given volumes"
+  (map
+    (fn [m] (select-keys m [:snapshot-id :volume-id :start-time]))
+    (get (describe-snapshots :filters [{:name "volume-id" :values volumes}]) :snapshots)
+    ))
 
 (defn -main []
   "The main function"
-  (pprint (get_snapshots_for_volumes (get_volumes_in_use)))
+  (def volumes_in_use (into #{} (get_volumes_in_use)))
+  (def snaps_in_use (get_snapshots_for_volumes volumes_in_use))
+  (def vols_with_snaps (into #{} (keys (group-by :volume-id snaps_in_use))))
+  (def vols_wo_snaps (difference volumes_in_use vols_with_snaps))
+  ;;TODO: filter the snaps by date
+  ;;
+  (println "In-use volumes without snapshots:")
+  (pprint vols_wo_snaps)
   )
 
