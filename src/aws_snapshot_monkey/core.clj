82673f051ff3789975eb54e7f86042d4340d62f8
jloope <james.loope@nike.com>
2014-07-28 13:02:44 -0700

    Add ability to snapshot the non-compliant volumes

--- src/aws_snapshot_monkey/core.clj
+++ src/aws_snapshot_monkey/core.clj
@@ -4,14 +4,10 @@
             [clj-time.local :as loc]
             [clj-time.format :as fmt]
             [clojure.tools.cli :as cli])
-  (:use clj-time.coerce)
+  (:use [clj-time.coerce])
   (:use [amazonica.aws.ec2])
   (:use [clojure.pprint])
   (:use [clojure.set]))
-
-(def opts [["-d" "--days-old" :default 30
-                                  :parse-fn #(Integer. %)
-                                  :assoc-fn max]])
 
 (defn get_volumes_in_use [] 
   "grab the aws volume-id's for in-use volumes"
@@ -31,20 +27,39 @@
 (defn filter_by_start_time [days_ago snapshots]
   "take a list of snapshots and return only those elements with a recent start
   time"
+  ;;TODO: Make sure they are completed?
   (map (fn [m] 
          (if (tm/before? (-> days_ago tm/days tm/ago) (get m :start-time))
           m ))
        snapshots))
 
+(defn snapshot_volumes [volumes]
+  "snapshot a list of volumes"
+  (println "Snapshotting volumes")
+  (map (fn [m]
+        (create-snapshot :volume-id m :Description "Snapshot Monkey"))
+        volumes
+        ))
+
 (defn -main [& args]
   "The main function"
+  (let  [[options args banner]
+         (cli/cli args
+                  ["-d" "--days-old" "Maximum age of a snapshot" :default 60
+                   :parse-fn #(Integer. %)])]
+
   (def volumes_in_use (into #{} (get_volumes_in_use)))
   (def snaps_in_use (get_snapshots_for_volumes volumes_in_use))
   (def vols_with_recent_snaps 
     (into #{} 
-          (keys (group-by :volume-id (filter_by_start_time 60 snaps_in_use)))))
+          (keys 
+            (group-by :volume-id 
+                      (filter_by_start_time 
+                        (:days-old options) snaps_in_use)))))
   (def vols_wo_snaps (difference volumes_in_use vols_with_recent_snaps))
-  (println "In-use volumes without recent snapshots:")
-  (pprint vols_wo_snaps)
-  )
+
+  ;; Snapshot the volumes that don't have recent snapshots
+  (snapshot_volumes vols_wo_snaps)
+
+  ))
 
