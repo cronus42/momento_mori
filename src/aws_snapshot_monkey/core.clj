34b2e0bce8a6162692ff30f28ebeba976a078cfe
jloope <james.loope@nike.com>
2014-07-30 14:44:46 -0700

    log the jobby; add region mangling to the credential thingie

--- src/aws_snapshot_monkey/core.clj
+++ src/aws_snapshot_monkey/core.clj
@@ -9,14 +9,15 @@
             [clojurewerkz.quartzite.triggers :as t]
             [clojurewerkz.quartzite.schedule.cron 
              :refer [schedule cron-schedule]]
-            [clojurewerkz.quartzite.conversion :as qc])
+            [clojurewerkz.quartzite.conversion :as qc]
+            [amazonica.core :refer [with-credential defcredential]])
   (:use [clj-time.coerce])
   (:use [amazonica.aws.ec2])
   (:use [clojure.pprint])
   (:use [clojure.set])
   (:use [clojure.walk]))
 
-
+;;TODO: specl testing
 ;;TODO: config file
 
 (defn get_volumes_in_use [] 
@@ -57,24 +58,25 @@
   (map (fn [m]
          (create-snapshot :volume-id m :Description "Snapshot Monkey"))
        volumes
-       ))
+       )
+  )
 
 (defjob snapshot_volumes_job [ctx]
   (let [options (keywordize-keys (qc/from-job-data ctx))]
-  (def volumes_in_use (get_volumes_in_use))
-  (def snaps_in_use (get_snapshots_for_volumes volumes_in_use))
-  (def snaps_defunct (difference (get_snapshots) volumes_in_use))
-  (def vols_with_recent_snaps 
-    (into #{} 
-          (keys 
-            (group-by :volume-id 
-                      (filter_by_start_time 
-                        (:days-old options) snaps_in_use)))))
-  (def vols_wo_snaps (difference volumes_in_use vols_with_recent_snaps))
+    (def volumes_in_use (get_volumes_in_use))
+    (def snaps_in_use (get_snapshots_for_volumes volumes_in_use))
+    (def snaps_defunct (difference (get_snapshots) volumes_in_use))
+    (def vols_with_recent_snaps 
+      (into #{} 
+            (keys 
+              (group-by :volume-id 
+                        (filter_by_start_time 
+                          (:days-old options) snaps_in_use)))))
+    (def vols_wo_snaps (difference volumes_in_use vols_with_recent_snaps))
 
-  (snapshot_volumes vols_wo_snaps)
+    (log/debug (snapshot_volumes vols_wo_snaps) )
 
-  ))
+    ))
 
 (defn -main [& args]
   "The main function"
@@ -84,10 +86,24 @@
                   ["-d" "--days-old" "Maximum age of a snapshot" :default 60
                    :parse-fn #(Integer. %)]
                   ["-f" "--frequency" "Run frequency in minutes" :default 30
-                   :parse-fn #(Integer. %)])]
+                   :parse-fn #(Integer. %)]
+                  ["-r" "--region" "AWS Region" :default "us-west-2"])]
+
+    ;;Dig the credentials out of JDK
+    (def aws_access_key_id 
+      (.getAWSAccessKeyId 
+        (.getCredentials (amazonica.core/get-credentials :cred))))
+    (def aws_secret_key 
+      (.getAWSSecretKey 
+        (.getCredentials (amazonica.core/get-credentials :cred))))
+
+    (defcredential aws_access_key_id aws_secret_key (:region options))
+
+
     ;;set up the scheduler
     (qs/initialize)
     (qs/start)
+    (log/info "Starting snapshot job with frequency of " (:frequency options))
     (let [job (j/build
                 (j/of-type snapshot_volumes_job)
                 (j/using-job-data options)
@@ -98,7 +114,7 @@
                     (t/with-schedule 
                       (schedule
                         (cron-schedule 
-                          (str "0 " (:frequency options) " * * * ?")))))]
+                          (str "0 */" (:frequency options) " * * * ?")))))]
 
       (qs/schedule job trigger)
       )
